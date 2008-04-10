f092cb0e83a23bbf4a6a074c6931ae1a281b7ea8
Andreas Kloeckner <inform@tiker.net>
2008-04-10 09:28:14 -0400

    Add license headers to Jesse Manning's files.
    OK'd by him via email on 2008-04-10.

--- boost/numeric/bindings/lapack/gelss.hpp
+++ boost/numeric/bindings/lapack/gelss.hpp
@@ -1,3 +1,11 @@
+//
+// Copyright Jesse Manning 2007
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef BOOST_NUMERIC_BINDINGS_LAPACK_GELSS_HPP
 #define BOOST_NUMERIC_BINDINGS_LAPACK_GELSS_HPP
 
