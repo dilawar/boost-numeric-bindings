563d6a400fecfe60253b51b80ed95b8b826693c1
Andreas Kloeckner <inform@tiker.net>
2009-03-31 11:34:04 -0400

    Get rid of warning by removing empty function. (Christoph Groth)

--- boost/numeric/bindings/lapack/hpsv.hpp
+++ boost/numeric/bindings/lapack/hpsv.hpp
@@ -152,12 +152,6 @@ namespace boost { namespace numeric { namespace bindings {
         LAPACK_ZHPTRF (&uplo, &n, traits::complex_ptr (ap), ipiv, info);
       }
 
-      template <typename HermA, typename IVec, typename Work>
-      inline
-      int hptrf (char const ul, HermA& a, IVec& i, Work& w, int const lw) {
-
-      }
-
     }
 
     template <typename HermA, typename IVec> 
